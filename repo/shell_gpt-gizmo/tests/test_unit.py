65e7cddb0a2170e219489af35786f88452bc2370
Dylan L.R. Pollock <admin@dlrp.ca>
2023-12-23 11:40:38 -0500

    Added cmd functions
    shell-gpt

---
+++ repo/shell_gpt-gizmo/tests/test_unit.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestMain(unittest.TestCase):
+    # TODO: Write these tests.
+
+    def test_main(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
