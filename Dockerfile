c7cfbe84b04e5bc1480be1b0bf0db071a623653a
Dylan L.R. Pollock <admin@dlrp.ca>
2023-12-27 01:43:09 -0500

    Dockerfile change
    Changes to dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,21 @@
 # Use Debian Trixie as the base image
 FROM debian:trixie
 
-# Update the package repository and upgrade the system. Then install Python and its tools.
+# Update the package repository and upgrade the system. Then install Python 3.11 and its tools.
 # Python3-venv is used for creating an isolated Python environment.
 RUN apt-get update && apt-get dist-upgrade -y && \
-    apt-get install -y python3 python3-pip python3-venv
+    apt-get install -y python3.11 python3-pip python3.11-venv
 
 # Create a virtual environment to isolate the Python environment
-RUN python3 -m venv /venv
+RUN python3.11 -m venv /venv
 
 # Activate virtual environment. All Python commands will now use this environment.
 ENV PATH="/venv/bin:$PATH"
+
+# Install required Python packages from requirements.txt
+# Note: Ensure requirements.txt is present in the context directory
+COPY requirements.txt /GenCore/
+RUN pip install -r /GenCore/requirements.txt
 
 # Copy the project files into the container. Adjust the source path if necessary.
 COPY . /GenCore
--- run.bat
+++ run.bat
@@ -3,58 +3,108 @@ SET PROJECT_PATH=C:\Users\admin\OneDrive\Desktop\GenCore
 SET CONTAINER_NAME=gencore-container
 SET IMAGE_NAME=gencore-app
 SET VOLUME_NAME=gencore-volume
+SET KUBE_CONFIG_PATH=C:\Users\admin\.kube\config
 
-echo [****Checking system prerequisites...****]
-python --version > NUL 2>&1
+echo [****Starting GenCore Deployment Process****]
+
+:: Checking system prerequisites
+echo [****Checking System Prerequisites...****]
+python --version 2>&1 | find "Python 3.11" > nul
 if %errorlevel% neq 0 (
-    echo [****Error: Python is not installed on the host OS. Please install Python 3.12.0 or higher.****]
+    echo [****Error: Python 3.11 is not installed on the host OS. Please install Python 3.11.****]
     exit /b 1
-)
-pip --version > NUL 2>&1
-if %errorlevel% neq 0 (
-    echo [****Error: Pip is not installed on the host OS. Please install Pip.****]
-    exit /b 1
+) else (
+    echo [****Python 3.11 Detected****]
 )
 
-echo [****Checking if Docker is running...****]
-docker info > NUL 2>&1
+:: Checking if Docker is running
+echo [****Verifying Docker Status...****]
+docker info > nul 2>&1
 if %errorlevel% neq 0 (
     echo [****Error: Docker is not running. Please start Docker and try again.****]
     exit /b 1
-)
-
-echo [****Building or updating Docker Image...****]
-docker build --no-cache -t %IMAGE_NAME% %PROJECT_PATH% || (
-    echo [****Error: Failed to build or update Docker Image.****]
-    exit /b 1
-)
-
-echo [****Ensuring Docker Volume exists...****]
-docker volume inspect %VOLUME_NAME% > NUL 2>&1 || docker volume create %VOLUME_NAME%
-
-echo [****Managing Docker Container...****]
-docker container inspect %CONTAINER_NAME% > NUL 2>&1
-if %errorlevel% neq 0 (
-    echo [****Creating and starting Docker Container...****]
-    docker run -d --name %CONTAINER_NAME% -v %VOLUME_NAME%:/data -v %PROJECT_PATH%:/GenCore --restart unless-stopped %IMAGE_NAME% || (
-        echo [****Error: Failed to create and start Docker Container.****]
-        exit /b 1
-    )
 ) else (
-    echo [****Updating and restarting existing Docker Container...****]
-    docker stop %CONTAINER_NAME% && docker rm %CONTAINER_NAME%
-    docker run -d --name %CONTAINER_NAME% -v %VOLUME_NAME%:/data -v %PROJECT_PATH%:/GenCore --restart unless-stopped %IMAGE_NAME% || (
-        echo [****Error: Failed to update and restart Docker Container.****]
-        exit /b 1
-    )
+    echo [****Docker is Running****]
 )
 
-echo [****Verifying Python environment inside Docker Container...****]
-docker exec %CONTAINER_NAME% python --version > NUL 2>&1
+:: Checking if Kubernetes is set up
+echo [****Checking if Kubernetes is set up...****]
+kubectl --kubeconfig=%KUBE_CONFIG_PATH% version --client > nul 2>&1
 if %errorlevel% neq 0 (
-    echo [****Error: Python is not correctly installed inside the Docker Container.****]
+    echo [****Error: Kubernetes is not set up correctly.****]
     exit /b 1
+) else (
+    echo [****Kubernetes Detected****]
 )
 
+:: Offering option to clean up Docker environment
+echo Would you like to clean up Docker environment before proceeding? (Y/N):
+set /p cleanup=<con
+if /I "%cleanup%"=="Y" (
+    echo [****Cleaning up Docker Environment...****]
+    docker system prune -af
+)
+
+:: Interactive Menu for User Actions
+:menu
+echo.
+echo Select an action:
+echo 1. Build and Run
+echo 2. Update
+echo 3. Clean up
+echo 4. Exit
+set /p action=<con
+
+if "%action%"=="1" goto build_run
+if "%action%"=="2" goto update
+if "%action%"=="3" goto cleanup
+if "%action%"=="4" exit /b 0
+echo [****Invalid Selection. Please choose a valid option.****]
+goto menu
+
+:build_run
+echo [****Building Docker Image: %IMAGE_NAME%...****]
+docker build --no-cache -t %IMAGE_NAME% %PROJECT_PATH% > build.log
+if %errorlevel% neq 0 (
+    echo [****Build Error: See build.log for details.****]
+    type build.log
+    exit /b 1
+) else (
+    echo [****Docker Image Built Successfully****]
+)
+
+echo [****Applying Kubernetes Configurations...****]
+kubectl --kubeconfig=%KUBE_CONFIG_PATH% apply -f %PROJECT_PATH%\gencore-deployment.yaml > kube.log
+if %errorlevel% neq 0 (
+    echo [****Error: Failed to apply Kubernetes configurations. Check kube.log for details.****]
+    type kube.log
+    exit /b 1
+) else (
+    echo [****Kubernetes Configurations Applied Successfully****]
+)
+
+echo [****Verifying Kubernetes Deployment...****]
+kubectl --kubeconfig=%KUBE_CONFIG_PATH% get deployments
+if %errorlevel% neq 0 (
+    echo [****Error: Kubernetes deployment verification failed.****]
+    exit /b 1
+) else (
+    echo [****Deployment Verified Successfully****]
+)
+goto end
+
+:update
+echo [****Updating...****]
+:: Update logic here (commented out for now)
+:: echo [****No Update Logic Defined****]
+goto end
+
+:cleanup
+echo [****Cleaning up...****]
+:: Cleanup logic here (commented out for now)
+:: echo [****No Cleanup Logic Defined****]
+goto end
+
+:end
 echo [****All operations completed successfully.****]
 pause
