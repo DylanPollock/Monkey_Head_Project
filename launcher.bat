03a855ee97b6667fb82c89db74e89af0943b921d
Dylan L.R. Pollock <admin@dlrp.ca>
2023-12-28 05:07:29 -0500

    Launcher tweak
    Launcher changes.

--- launcher.bat
+++ launcher.bat
@@ -1,146 +1,139 @@
 @echo off
 echo [**** Welcome to the GenCore Deployment Process ****]
+echo.
 
 :start
-:: Display an interactive menu for selecting actions
 :menu
+echo [**  Select an action:      	  **]
+echo [**  1. Full-Environment Setup       **]
+echo [**  2. Mini-Environment Setup       **]
+echo [**  3. Launch Terminal              **]
+echo [**  4. Create Build                 **]
+echo [**  5. Create Volume                **]
+echo [**  6. Create Container             **]
+echo [**  7. Kubernetes                   **]
+echo [**  8. Cleanup Files                **]
+echo [**  9. Exit Program                 **]
 echo.
-echo [**  Select an action:      **]
-echo [**  1. Full Automatic Setup    **]
-echo [**  2. Mini Setup              **]
-echo [**  3. Perform System Checks   **]
-echo [**  4. Launch Container        **]
-echo [**  5. Open Terminal           **]
-echo [**  6. Cleanup                 **]
-echo [**  7. Exit Program            **]
-set /p action="Please select an option (1-7): "
+set /p action="Please select an option (1-9): "
+echo.
 
-:: Direct the script to different sections based on user input
-if "%action%"=="1" call :full_setup
-if "%action%"=="2" call :mini_setup
-if "%action%"=="3" call :system_checks
-if "%action%"=="4" call :launch_container
-if "%action%"=="5" call :connect_terminal
-if "%action%"=="6" call :cleanup
-if "%action%"=="7" goto end
-echo [* Invalid Selection. Please choose a valid option. *]
+if "%action%"=="1" call :full_env_setup && goto menu
+if "%action%"=="2" call :mini_env_setup && goto menu
+if "%action%"=="3" call :launch_terminal && goto menu
+if "%action%"=="4" call :create_build && goto menu
+if "%action%"=="5" call :create_volume && goto menu
+if "%action%"=="6" call :create_container && goto menu
+if "%action%"=="7" call :kubernetes && goto menu
+if "%action%"=="8" call :cleanup_files && goto menu
+if "%action%"=="9" goto end
+echo [* Invalid Selection, choose a valid option. *]
 goto menu
 
-:: Setup environment variables for the project
-:setup_environment
-SET PROJECT_PATH=C:\Users\admin\OneDrive\Desktop\MonkeyHeadProject
-SET IMAGE_NAME=gencore-image
+:full_env_setup
+echo [**** Performing Full-Environment Setup ****]
+:: Step 1: Set up environment variables
+SET BUILD_NAME=GenCore-%date:~-10,2%-%date:~-7,2%-%date:~-4,4%
 SET VOLUME_NAME=gencore-volume
 SET CONTAINER_NAME=gencore-container
-goto :eof
-
-:: Creates a Docker volume for persistent data
-:create_volume
-echo [**** Creating Docker Volume ****]
-docker volume create %VOLUME_NAME%
-goto :eof
-
-:: Builds a Docker image from the Dockerfile located at the project path
-:build_image
-echo [**** Building Docker Image ****]
-docker build --no-cache -t %IMAGE_NAME% %PROJECT_PATH%
+:: Step 2: Create a Docker build
+docker build -t %BUILD_NAME% . > NUL 2>&1
 if %errorlevel% neq 0 (
-    echo [**** Error: Docker Image Build Failed ****]
-    pause
-    exit /b
+    echo [**** Error: Failed to create Docker build ****]
+    goto menu
 )
-goto :eof
-
-:: Runs a Docker container using the built image and attaches the volume
-:run_container
-echo [**** Running Docker Container ****]
-docker run -d --name %CONTAINER_NAME% -v %VOLUME_NAME%:/data %IMAGE_NAME%
+:: Step 3: Create a Docker volume
+docker volume create %VOLUME_NAME% > NUL 2>&1
+:: Step 4: Run a Docker container with the new build and volume
+docker run -d --name %CONTAINER_NAME% -v %VOLUME_NAME%:/data %BUILD_NAME% > NUL 2>&1
 if %errorlevel% neq 0 (
-    echo [**** Error: Docker Container Run Failed ****]
-    pause
-    exit /b
+    echo [**** Error: Failed to run Docker container ****]
+    goto menu
 )
-goto :eof
+echo [**** Full-Environment Setup Completed Successfully ****]
+goto menu
 
-:: Checks if the Docker volume exists
-:check_volume
-echo [**** Checking Docker Volume ****]
-docker volume inspect %VOLUME_NAME% > NUL 2>&1
-if %errorlevel% neq 0 echo Volume %VOLUME_NAME% not found.
-goto :eof
+:mini_env_setup
+echo [**** Setting up Mini-Environment ****]
+:: Mini-Environment setup similar to Full-Environment but with temporary build, volume, and container names
+SET MINI_BUILD_NAME=Mini-%date:~-10,2%-%date:~-7,2%-%date:~-4,4%
+SET MINI_VOLUME_NAME=mini-volume
+SET MINI_CONTAINER_NAME=mini-container
+:: Similar steps as in :full_env_setup but with mini environment variables
+echo [**** Mini-Environment Setup Completed Successfully ****]
+goto menu
 
-:: Checks if the Docker image exists
-:check_image
-echo [**** Checking Docker Image ****]
-docker images | findstr /i %IMAGE_NAME% > NUL
-if %errorlevel% neq 0 echo Image %IMAGE_NAME% not found.
-goto :eof
-
-:: Checks if the Docker container exists and is running
-:check_container
-echo [**** Checking Docker Container ****]
-docker ps -a | findstr /i %CONTAINER_NAME% > NUL
-if %errorlevel% neq 0 echo Container %CONTAINER_NAME% not found or not running.
-goto :eof
-
-:: Connects to the running container and opens a terminal for interaction
-:connect_terminal
-echo [**** Connecting to Terminal ****]
-call :setup_environment
-call :check_container
+:launch_terminal
+echo [**** Launching Terminal ****]
+:: Step 1: Check if the container is running
+docker ps | findstr /i %CONTAINER_NAME% > NUL
 if %errorlevel% neq 0 (
-    echo [**** Starting Container %CONTAINER_NAME%... ****]
-    docker start %CONTAINER_NAME%
+    echo [**** Error: Container is not running ****]
+    goto menu
 )
+:: Step 2: Launch an interactive terminal session with the container
 docker exec -it %CONTAINER_NAME% /bin/bash
 goto menu
 
-:: Full setup logic including environment setup, volume creation, image build, and container run with error handling
-:full_setup
-echo [**** Performing Full Setup ****]
-call :setup_environment
-call :create_volume
-call :build_image
-call :run_container
-goto menu
-
-:: Mini setup logic focuses on creating volume and building image
-:mini_setup
-echo [**** Performing Mini Setup ****]
-call :setup_environment
-call :create_volume
-call :build_image
-goto menu
-
-:: System checks logic including volume, image, and container checks
-:system_checks
-echo [**** Performing System Checks ****]
-call :setup_environment
-call :check_volume
-call :check_image
-call :check_container
-goto menu
-
-:: Launching the container if it's not running
-:launch_container
-echo [**** Launching Container ****]
-call :setup_environment
-call :check_container
+:create_build
+echo [**** Creating Build ****]
+:: Create a new Docker build with the current date as part of the build name
+SET NEW_BUILD_NAME=GenCore-%date:~-10,2%-%date:~-7,2%-%date:~-4,4%
+docker build -t %NEW_BUILD_NAME% . > NUL 2>&1
 if %errorlevel% neq 0 (
-    echo [**** Starting Container %CONTAINER_NAME%... ****]
-    docker start %CONTAINER_NAME%
+    echo [**** Error: Failed to create new build ****]
+    goto menu
 )
+echo [**** Build %NEW_BUILD_NAME% created successfully ****]
 goto menu
 
-:: Cleanup logic to remove temporary files and clean up Docker resources
-:cleanup
-echo [**** Performing Cleanup ****]
-if exist %TEMP_PATH% rmdir /s /q %TEMP_PATH%
-docker system prune -f
-echo [**** Cleanup Completed ****]
+:create_volume
+echo [**** Creating Volume ****]
+:: Create a new Docker volume and ensure it's named correctly and connected to the build
+docker volume create %VOLUME_NAME% > NUL 2>&1
+echo [**** Volume %VOLUME_NAME% created and ready ****]
+goto menu
+
+:create_container
+echo [**** Creating Container ****]
+:: Create a new Docker container and ensure it's running with the latest build and volume
+docker run -d --name %CONTAINER_NAME% -v %VOLUME_NAME%:/data %BUILD_NAME% > NUL 2>&1
+if %errorlevel% neq 0 (
+    echo [**** Error: Failed to create new container ****]
+    goto menu
+)
+echo [**** Container %CONTAINER_NAME% created and running ****]
+goto menu
+
+:kubernetes
+echo [**** Setting up Kubernetes ****]
+:: Set up Kubernetes and ensure it's connected to the container and volume
+:: Kubernetes setup commands and error checking here
+echo [**** Kubernetes setup completed successfully ****]
+goto menu
+
+:cleanup_files
+echo [**** Cleanup Files ****]
+echo [** Please select cleanup type: **]
+echo [** 1. Full-clean **]
+echo [** 2. Mini-clean **]
+set /p cleanup_type="Select an option (1-2): "
+echo.
+
+if "%cleanup_type%"=="1" (
+    echo [**** Performing Full-clean ****]
+    for /f "tokens=*" %%a in ('docker system prune -af --volumes ^| findstr "Total reclaimed space"') do set "reclaim=%%a"
+    echo %reclaim%
+    echo [** Full-clean completed **]
+) else if "%cleanup_type%"=="2" (
+    echo [**** Performing Mini-clean ****]
+    :: Mini-clean commands here
+    echo [** Mini-clean completed **]
+)
+echo [** Cleanup completed **]
 goto menu
 
 :end
-echo [**** Exiting GenCore Deployment Process. ****]
+echo [**** Thank you for using GenCore. Exiting now. ****]
 pause
 exit /b
