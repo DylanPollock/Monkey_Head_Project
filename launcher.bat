8cb1f403556426fb5a7e35b84e612fd3880bafe3
Dylan L.R. Pollock <admin@dlrp.ca>
2023-12-28 13:00:40 -0500

    Fix bug in login functionality

--- launcher.bat
+++ launcher.bat
@@ -1,117 +1,390 @@
 @echo off
 cls
 color 0A
-echo [****| GenCore Deployment Process |****]
+echo [****|     GenCore Deployment Process   |****]
 echo.
 
 :start
 :menu
 cls
-echo [****|  1. Full-Environment Setup        |****]
-echo [****|  2. Mini-Environment Setup        |****]
-echo [****|  3. Launch Terminal               |****]
-echo [****|  4. Create Build                  |****]
-echo [****|  5. Create Volume                 |****]
-echo [****|  6. Create Container              |****]
-echo [****|  7. Kubernetes                    |****]
-echo [****|  8. Cleanup Files                 |****]
-echo [****|  9. Update Python & Debian        |****]
-echo [****|  10. Exit Program                 |****]
+echo [****|     1. Full Setup                |****]
+echo [****|     2. 'Mini' Setup              |****]
+echo [****|     3. Cleanup Files             |****]
+echo [****|     4. Launch Terminal           |****]
+echo [****|     5. Update Python & Debian    |****]
+echo [****|     6. Create Build              |****]
+echo [****|     7. Create Volume             |****]
+echo [****|     8. Create Container          |****]
+echo [****|     9. Kubernetes                |****]
+echo [****|    10. Exit Program              |****]
 echo.
 
 set /p action="Please select an option (1-10): "
 echo.
 
-if "%action%"=="1" goto full_env_setup
-if "%action%"=="2" goto mini_env_setup
-if "%action%"=="3" goto launch_terminal
-if "%action%"=="4" goto create_build
-if "%action%"=="5" goto create_volume
-if "%action%"=="6" goto create_container
-if "%action%"=="7" goto kubernetes
-if "%action%"=="8" goto cleanup_files
-if "%action%"=="9" goto update_python_debian
+if "%action%"=="1" goto full_setup
+if "%action%"=="2" goto mini_setup
+if "%action%"=="3" goto cleanup_files
+if "%action%"=="4" goto launch_terminal
+if "%action%"=="5" goto update_python_debian
+if "%action%"=="6" goto create_build
+if "%action%"=="7" goto create_volume
+if "%action%"=="8" goto create_container
+if "%action%"=="9" goto kubernetes
 if "%action%"=="10" goto end
 
-echo [****| Invalid Selection. Please choose a valid option. |****]
+echo [****|     Invalid Selection. Choose a valid option:   |****]
 pause
 goto menu
 
-:full_env_setup
+:full_setup
 cls
-echo [****| Performing Full-Environment Setup |****]
-:: Full-Environment Setup commands here. Example:
-:: Install necessary software
-:: Set environment variables
-:: Download and configure necessary files
+echo [****|     Performing Full Setup   |****]
+
+:: Step 1: Install necessary software
+echo [****| Step 1: Installing required software |****]
+:: Using 'apt-get' for Debian-based systems to install software packages.
+apt-get update
+apt-get install -y software-package-name1 software-package-name2
+
+:: Check the exit code of the installation command
+if %errorlevel% neq 0 (
+    echo [****| Error during software installation |****]
+    echo [****| Exiting Full Setup.              |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 1: Required software installed successfully |****]
+)
+
+:: Step 2: Set environment variables
+echo [****| Step 2: Setting environment variables |****]
+:: Setting environment variables.
+set PROJECT=GenCore
+set ENVIRONMENT=Production
+
+:: Check the exit code of the environment variable setting
+if %errorlevel% neq 0 (
+    echo [****| Error during environment variable setup |****]
+    echo [****| Exiting Full Setup.                    |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 2: Environment variables set successfully |****]
+)
+
+:: Step 3: Download necessary files
+echo [****| Step 3: Downloading necessary files |****]
+:: Using 'curl' to download files from the internet.
+curl -o example_file.ext https://example.com/path/to/file.ext
+
+:: Check the exit code of the download step
+if %errorlevel% neq 0 (
+    echo [****| Error during file download |****]
+    echo [****| Exiting Full Setup.         |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 3: Necessary files downloaded successfully |****]
+)
+
+:: Return to the main menu after setup is complete
+goto full_setup_completed
+
+:full_setup_completed
+cls
+echo [****| Full Setup completed successfully |****]
 pause
 goto menu
 
-:mini_env_setup
+:mini_setup
 cls
-echo [****| Setting up Mini-Environment |****]
-:: Mini-Environment Setup commands here. Example:
-:: Install minimal necessary software
-:: Configure specific environment settings
-pause
-goto menu
+echo [****| Setting up 'Mini' Environment |****]
 
-:launch_terminal
-cls
-echo [****| Launching Terminal |****]
-:: Command to open a new terminal window
-start cmd /k
-pause
-goto menu
+:: Step 1: Install minimal necessary software
+echo [****| Step 1: Installing minimal required software |****]
+:: Using 'apt-get' for Debian-based systems to install minimal necessary software packages.
+apt-get update
+apt-get install -y minimal-software-package-name1 minimal-software-package-name2
 
-:create_build
-cls
-echo [****| Creating Build |****]
-:: Commands for build creation. Example:
-:: Compile code or assemble resources
-pause
-goto menu
+:: Check the exit code of the installation command
+if %errorlevel% neq 0 (
+    echo [****| Error during minimal software installation |****]
+    echo [****| Exiting 'Mini' Environment Setup.           |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 1: Minimal required software installed successfully |****]
+)
 
-:create_volume
-cls
-echo [****| Creating Volume |****]
-:: Commands for volume creation. Example:
-:: Initialize and configure storage volumes
-pause
-goto menu
+:: Step 2: Configure specific settings for the 'Mini' Environment
+echo [****| Step 2: Configuring specific settings |****]
+:: Configuration commands for the 'Mini' environment.
+:: Example: set MINI_ENV_SETTING=value
 
-:create_container
-cls
-echo [****| Creating Container |****]
-:: Commands for container creation. Example:
-:: Deploy and start Docker containers
-pause
-goto menu
+:: Check the exit code of the configuration step
+if %errorlevel% neq 0 (
+    echo [****| Error during configuration for 'Mini' Environment |****]
+    echo [****| Exiting 'Mini' Environment Setup.                 |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 2: Configuration for 'Mini' Environment completed successfully |****]
+)
 
-:kubernetes
+:: Return to the main menu after 'Mini' environment setup is complete
+goto mini_setup_completed
+
+:mini_setup_completed
 cls
-echo [****| Kubernetes Setup |****]
-:: Kubernetes setup commands. Example:
-:: Initialize Kubernetes cluster
-:: Deploy services and applications
+echo [****| 'Mini' Environment Setup completed successfully |****]
 pause
 goto menu
 
 :cleanup_files
 cls
 echo [****| Cleaning up Files |****]
-:: Commands for file cleanup. Example:
-:: Remove temporary files
-:: Clear cache or log files
+
+:: Step 1: Execute cleanup commands
+echo [****| Step 1: Executing cleanup commands |****]
+:: Cleanup commands, such as removing temporary files, clearing caches, or deleting log files.
+del /f /q C:\path\to\temp\*.tmp
+del /f /q C:\path\to\logs\*.log
+
+:: Additional cleanup commands can be added here.
+:: For instance, clearing browser cache or other application-specific temporary files.
+
+:: Check the exit code of the cleanup commands
+if %errorlevel% neq 0 (
+    echo [****| Error during cleanup process |****]
+    echo [****| Exiting Cleanup Files.      |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 1: Cleanup commands executed successfully |****]
+)
+
+:: Return to the main menu after cleanup is complete
+goto cleanup_completed
+
+:cleanup_completed
+cls
+echo [****| Cleanup completed successfully |****]
+pause
+goto menu
+
+
+:launch_terminal
+cls
+echo [****| Launching Terminal |****]
+
+:: Step 1: Launch the terminal
+echo [****| Step 1: Launching terminal |****]
+:: Use the 'start' command to open a new command prompt window with the `/k` option to keep the window open after execution.
+start cmd /k
+
+:: Check the exit code of the launch command
+if %errorlevel% neq 0 (
+    echo [****| Error launching terminal |****]
+    echo [****| Exiting Launch Terminal. |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 1: Terminal launched successfully |****]
+)
+
+:: Return to the main menu after launching the terminal
+goto launch_terminal_completed
+
+:launch_terminal_completed
+cls
+echo [****| Terminal launched successfully |****]
 pause
 goto menu
 
 :update_python_debian
 cls
 echo [****| Updating Python & Debian |****]
-:: Commands for updates. Example:
-:: Update Python packages
-:: Apply system updates for Debian
+
+:: Step 1: Update Python packages
+echo [****| Step 1: Updating Python packages |****]
+:: Using 'pip' to update Python and related packages.
+pip install --upgrade pip
+pip install --upgrade setuptools
+
+:: Check the exit code of the Python package update command
+if %errorlevel% neq 0 (
+    echo [****| Error updating Python packages |****]
+    echo [****| Exiting Update Python & Debian. |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 1: Python packages updated successfully |****]
+)
+
+:: Step 2: Apply Debian system updates
+echo [****| Step 2: Applying Debian system updates |****]
+:: Using 'apt-get' to update the Debian system.
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+
+:: Check the exit code of the Debian system update command
+if %errorlevel% neq 0 (
+    echo [****| Error applying Debian system updates |****]
+    echo [****| Exiting Update Python & Debian. |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 2: Debian system updates applied successfully |****]
+)
+
+:: Return to the main menu after update is complete
+goto update_python_debian_completed
+
+:update_python_debian_completed
+cls
+echo [****| Python & Debian updated successfully |****]
+pause
+goto menu
+
+:create_build
+cls
+echo [****| Creating Build |****]
+
+:: Step 1: Compile code and assemble resources
+echo [****| Step 1: Compiling code and assembling resources |****]
+:: Example build commands (customize these commands according to your build process)
+:: For compiling a Java application
+:: javac MyApplication.java
+
+:: For a .NET application
+:: msbuild /t:build /p:Configuration=Release MySolution.sln
+
+:: For a Node.js application
+:: npm run build
+
+:: For Python applications, a build step may not be required, but you can include any pre-deployment tasks.
+:: Example: python setup.py install
+
+:: Check the exit code of the build command
+if %errorlevel% neq 0 (
+    echo [****| Error during build creation |****]
+    echo [****| Exiting Build Creation. |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 1: Build created successfully |****]
+)
+
+:: Return to the main menu after build creation is complete
+goto create_build_completed
+
+:create_build_completed
+cls
+echo [****| Build created successfully |****]
+pause
+goto menu
+
+:create_volume
+cls
+echo [****| Creating Volume |****]
+
+:: Step 1: Initialize and configure storage volumes
+echo [****| Step 1: Initializing and configuring storage volumes |****]
+:: Example command to create a Docker volume
+:: docker volume create my_volume
+
+:: For Windows disk volumes, you might use diskpart or PowerShell commands
+:: Example using diskpart (this requires a script file or manual input)
+:: diskpart /s C:\path\to\diskpart_script.txt
+
+:: Example using PowerShell to create and format a new volume (Admin privileges required)
+:: powershell -Command "New-Volume -DiskNumber 1 -FileSystem NTFS -FriendlyName 'MyVolume'"
+
+:: Check the exit code of the volume creation command
+if %errorlevel% neq 0 (
+    echo [****| Error during volume creation |****]
+    echo [****| Exiting Volume Creation. |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 1: Volume created successfully |****]
+)
+
+:: Return to the main menu after volume creation is complete
+goto create_volume_completed
+
+:create_volume_completed
+cls
+echo [****| Volume created successfully |****]
+pause
+goto menu
+
+
+:create_container
+cls
+echo [****| Creating Container |****]
+
+:: Step 1: Deploy and start Docker containers
+echo [****| Step 1: Deploying and starting Docker containers |****]
+:: Example command to pull and run a Docker container
+:: docker pull myimage:latest
+:: docker run -d --name mycontainer myimage:latest
+
+:: For more complex configurations, you can use docker-compose
+:: Example: docker-compose up -d
+
+:: Check the exit code of the container creation command
+if %errorlevel% neq 0 (
+    echo [****| Error during container creation |****]
+    echo [****| Exiting Container Creation. |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 1: Container created and running successfully |****]
+)
+
+:: Return to the main menu after container creation is complete
+goto create_container_completed
+
+:create_container_completed
+cls
+echo [****| Container created and running |****]
+pause
+goto menu
+
+:kubernetes
+cls
+echo [****| Kubernetes Setup |****]
+
+:: Step 1: Initialize Kubernetes cluster and deploy services
+echo [****| Step 1: Initializing Kubernetes cluster and deploying services |****]
+:: Example command to initialize a Kubernetes cluster using Minikube (for local development)
+:: minikube start
+
+:: Example command to deploy a service or application using kubectl
+:: kubectl apply -f myservice-deployment.yaml
+
+:: For more complex configurations, you might use Helm charts
+:: Example: helm install myservice ./my-helm-chart
+
+:: Check the exit code of the Kubernetes setup command
+if %errorlevel% neq 0 (
+    echo [****| Error during Kubernetes setup |****]
+    echo [****| Exiting Kubernetes Setup. |****]
+    pause
+    goto menu
+) else (
+    echo [****| Step 1: Kubernetes setup completed successfully |****]
+)
+
+:: Return to the main menu after Kubernetes setup is complete
+goto kubernetes_setup_completed
+
+:kubernetes_setup_completed
+cls
+echo [****| Kubernetes setup completed successfully |****]
 pause
 goto menu
 
