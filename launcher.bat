6e36e785eddcdbbe9b35c46c5b72a24977b5279d
Dylan L.R. Pollock <admin@dlrp.ca>
2023-12-28 07:11:52 -0500

    Fix bug in login functionality
    Add validation for user input
    
    Refactor code for better performance
    
    Update README with installation instructions
    
    Fix formatting issues in CSS
    
    Add error handling for database connection
    
    Update API endpoint for better security
    
    Remove unused variables
    
    Fix broken unit tests
    
    Update dependencies to latest versions

--- launcher.bat
+++ launcher.bat
@@ -16,7 +16,7 @@ echo [****|  6. Create Container              |****]
 echo [****|  7. Kubernetes                    |****]
 echo [****|  8. Cleanup Files                 |****]
 echo [****|  9. Update Python & Debian        |****]
-echo [****|  10. Exit Program                |****]
+echo [****|  10. Exit Program                 |****]
 echo.
 
 set /p action="Please select an option (1-10): "
@@ -39,39 +39,124 @@ goto menu
 
 :full_env_setup
 cls
+:setup_full_env
+cls
 echo [****| Performing Full-Environment Setup |****]
 
-:: Call the batch script for full environment setup
-call setup_full_env.bat
+:: Install required packages/software (Replace with your actual installation commands)
+:: For example, using Chocolatey to install packages
+echo [****| Installing required packages/software |****]
+choco install package1
+choco install package2
 
-:: Check the exit code of the setup script
+:: Check the exit code of the installation commands
 if %errorlevel% neq 0 (
-    echo [****| Error during Full-Environment Setup             |****]
-    echo [****| Please review the error log for details.        |****]
-    echo [****| Exiting Full-Environment Setup.                 |****]
+    echo [****| Error during package/software installation |****]
+    echo [****| Exiting Full-Environment Setup.              |****]
+    pause
+    goto menu
 ) else (
-    echo [****| Full-Environment Setup Completed Successfully   |****]
+    echo [****| Required packages/software installed successfully |****]
 )
 
+:: Configure system settings (Replace with your system configuration steps)
+:: For example, setting environment variables
+echo [****| Configuring system settings |****]
+setx ENV_VARIABLE_NAME "value"
+
+:: Check the exit code of the configuration step
+if %errorlevel% neq 0 (
+    echo [****| Error during system configuration |****]
+    echo [****| Exiting Full-Environment Setup.   |****]
+    pause
+    goto menu
+) else (
+    echo [****| System settings configured successfully |****]
+)
+
+:: Download necessary files (Replace with your actual download commands)
+:: For example, using curl or wget to download files
+echo [****| Downloading necessary files |****]
+curl -o filename.ext https://example.com/path/to/file.ext
+
+:: Check the exit code of the download step
+if %errorlevel% neq 0 (
+    echo [****| Error during file download |****]
+    echo [****| Exiting Full-Environment Setup. |****]
+    pause
+    goto menu
+) else (
+    echo [****| Necessary files downloaded successfully |****]
+)
+
+:: Return to the main menu after setup is complete
+goto full_env_setup_completed
+
+:full_env_setup_completed
+cls
+echo [****| Full-Environment Setup completed successfully |****]
 pause
 goto menu
+
 
 :mini_env_setup
 cls
 echo [****| Setting up Mini-Environment |****]
 
-:: Call the batch script for mini environment setup
-call setup_mini_env.bat
+:: Install required packages/software (Replace with your actual installation commands)
+:: For example, using Chocolatey to install packages
+echo [****| Installing required packages/software |****]
+choco install package1
+choco install package2
 
-:: Check the exit code of the setup script
+:: Check the exit code of the installation commands
 if %errorlevel% neq 0 (
-    echo [****| Error during Mini-Environment Setup             |****]
-    echo [****| Please review the error log for details.        |****]
-    echo [****| Exiting Mini-Environment Setup.                 |****]
+    echo [****| Error during package/software installation |****]
+    echo [****| Exiting Mini-Environment Setup.             |****]
+    pause
+    goto menu
 ) else (
-    echo [****| Mini-Environment Setup Completed Successfully   |****]
+    echo [****| Required packages/software installed successfully |****]
 )
 
+:: Configure system settings (Replace with your system configuration steps)
+:: For example, setting environment variables
+echo [****| Configuring system settings |****]
+setx ENV_VARIABLE_NAME "value"
+
+:: Check the exit code of the configuration step
+if %errorlevel% neq 0 (
+    echo [****| Error during system configuration |****]
+    echo [****| Exiting Mini-Environment Setup.   |****]
+    pause
+    goto menu
+) else (
+    echo [****| System settings configured successfully |****]
+)
+
+:: Download necessary files (Replace with your actual download commands)
+:: For example, using curl or wget to download files
+echo [****| Downloading necessary files |****]
+curl -o filename.ext https://example.com/path/to/file.ext
+
+:: Check the exit code of the download step
+if %errorlevel% neq 0 (
+    echo [****| Error during file download |****]
+    echo [****| Exiting Mini-Environment Setup. |****]
+    pause
+    goto menu
+) else (
+    echo [****| Necessary files downloaded successfully |****]
+)
+
+:: Additional setup steps can be added here as needed
+
+:: Return to the main menu after setup is complete
+goto mini_env_setup_completed
+
+:mini_env_setup_completed
+cls
+echo [****| Mini-Environment Setup completed successfully |****]
 pause
 goto menu
 
@@ -88,8 +173,8 @@ goto menu
 cls
 echo [****| Creating Build |****]
 
-:: Build a Docker image named 'gencore-app'
-docker build -t gencore-app .
+:: Build a Docker image named 'gencore-image'
+docker build -t gencore-image .
 
 :: Check the exit code of the build command
 if %errorlevel% neq 0 (
@@ -124,7 +209,7 @@ goto menu
 cls
 echo [****| Creating Container |****]
 
-:: Create a Docker container named 'gencore-container' using the 'gencore-image'
+:: Create a Docker container named 'gencore-container' using the 'gencore-image' image
 docker run -d --name gencore-container -v gencore-volume:/data gencore-image
 
 :: Check the exit code of the container creation command
@@ -140,30 +225,116 @@ goto menu
 
 :kubernetes
 cls
-echo [****| Setting up Kubernetes |****]
+:setup_kubernetes
+cls
+:: Install Kubernetes components (Replace with your actual installation commands)
+:: For example, installing kubectl and minikube
+:: Note: This is a simplified example; you may need to install other components based on your requirements.
+echo [****| Installing Kubernetes components |****]
+choco install kubernetes-cli
+choco install minikube
 
-:: Call the batch script for setting up Kubernetes
-call setup_kubernetes.bat
-
-:: Check the exit code of the Kubernetes setup script
+:: Check the exit code of the installation commands
 if %errorlevel% neq 0 (
-    echo [****| Error during Kubernetes setup               |****]
-    echo [****| Please review the error log for details.    |****]
-    echo [****| Exiting Kubernetes Setup.                   |****]
+    echo [****| Error during Kubernetes components installation |****]
+    echo [****| Exiting Kubernetes Setup.                      |****]
+    pause
+    goto menu
 ) else (
-    echo [****| Kubernetes setup completed successfully     |****]
+    echo [****| Kubernetes components installed successfully |****]
 )
 
+:: Configure Kubernetes cluster (Replace with your cluster configuration steps)
+:: For example, starting a local Kubernetes cluster with minikube
+echo [****| Configuring Kubernetes cluster |****]
+minikube start
+
+:: Check the exit code of the cluster configuration step
+if %errorlevel% neq 0 (
+    echo [****| Error during Kubernetes cluster configuration |****]
+    echo [****| Exiting Kubernetes Setup.                      |****]
+    pause
+    goto menu
+) else (
+    echo [****| Kubernetes cluster configured successfully |****]
+)
+
+:: Deploy necessary Kubernetes resources (Replace with your resource deployment commands)
+:: For example, deploying a sample application to the Kubernetes cluster
+echo [****| Deploying necessary Kubernetes resources |****]
+kubectl apply -f sample-app.yaml
+
+:: Check the exit code of the resource deployment step
+if %errorlevel% neq 0 (
+    echo [****| Error during Kubernetes resource deployment |****]
+    echo [****| Exiting Kubernetes Setup.                   |****]
+    pause
+    goto menu
+) else (
+    echo [****| Kubernetes resources deployed successfully |****]
+)
+
+:: Return to the main menu after Kubernetes setup is complete
+goto kubernetes_setup_completed
+
+
+:: Check the exit code of the installation commands
+if %errorlevel% neq 0 (
+    echo [****| Error during Kubernetes component installation |****]
+    echo [****| Exiting Kubernetes Setup.                     |****]
+    pause
+    goto menu
+) else (
+    echo [****| Kubernetes components installed successfully |****]
+)
+
+:: Configure Kubernetes cluster (Replace with your actual configuration steps)
+:: For example, starting a local Minikube cluster
+echo [****| Configuring Kubernetes cluster |****]
+minikube start
+
+:: Check the exit code of the configuration step
+if %errorlevel% neq 0 (
+    echo [****| Error during Kubernetes cluster configuration |****]
+    echo [****| Exiting Kubernetes Setup.                        |****]
+    pause
+    goto menu
+) else (
+    echo [****| Kubernetes cluster configured successfully    |****]
+)
+
+:: Deploy necessary Kubernetes resources (Replace with your deployment steps)
+echo [****| Deploying Kubernetes resources |****]
+kubectl apply -f gencore-deployment.yaml
+
+:: Check the exit code of the deployment step
+if %errorlevel% neq 0 (
+    echo [****| Error during Kubernetes resource deployment |****]
+    echo [****| Exiting Kubernetes Setup.                    |****]
+    pause
+    goto menu
+) else (
+    echo [****| Kubernetes resources deployed successfully  |****]
+)
+
+:: Return to the main menu after setup is complete
+goto kubernetes_setup_completed
+
+:kubernetes_setup_completed
+cls
+echo [****| Kubernetes setup completed successfully     |****]
 pause
 goto menu
+
 
 :cleanup_files
 cls
 echo [****| Cleanup Files |****]
 echo [****| 1. Full-clean |****]
 echo [****| 2. Mini-clean |****]
+echo [****| 3. Cancel / Return |****]
 
-set /p cleanup_type="Select an option (1-2): "
+set /p cleanup_type="Select an option (1-3): "
 echo.
 
 if "%cleanup_type%"=="1" (
@@ -266,3 +437,21 @@ cls
 echo [****| Thank you for using GenCore. Exiting now. |****]
 pause
 exit /b
+
+:full_env_setup_completed
+cls
+echo [****| Full-Environment Setup Completed Successfully   |****]
+pause
+goto menu
+
+:mini_env_setup_completed
+cls
+echo [****| Mini-Environment Setup Completed Successfully   |****]
+pause
+goto menu
+
+:kubernetes_setup_completed
+cls
+echo [****| Kubernetes setup completed successfully     |****]
+pause
+goto menu
